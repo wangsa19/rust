b28c1e9ba3f8a3073f9dba7e4465cda2c2df9d33
wangsa <dewanggawpw@gmail.com>
2025-09-07 22:24:22 +0700

    feat: the slice type

---
+++ the-slice-type/src/main.rs
@@ -0,0 +1,94 @@
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
+
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
+
+//     s.len()
+// }
+
+// fn main() {
+//     let s = String::from("hello world");
+
+//     let word = first_word(&s);
+
+//     println!("Indeks spasi pertama adalah: {}", word);
+// }
+
+// fn main() {
+//     let s = String::from("hello");
+
+//     let slice = &s[0..2];
+//     let slice = &s[..2];
+
+//     println!("{}", slice);
+// }
+
+// fn first_word(s: &String) -> &str {
+//     let bytes = s.as_bytes();
+
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return &s[0..i];
+//         }
+//     }
+
+//     &s[..]
+// }
+
+// fn main() {
+//     let mut s = String::from("hello world");
+
+//     let word = first_word(&s);
+
+//     s.clear(); // error!
+
+//     println!("the first word is: {word}");
+// }
+
+// fn first_word(s: &str) -> &str {
+//     let bytes = s.as_bytes();
+
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return &s[0..i];
+//         }
+//     }
+
+//     &s[..]
+// }
+
+// fn main() {
+//     let my_string = String::from("hello world");
+
+//     // `first_word` works on slices of `String`s, whether partial or whole.
+//     let word = first_word(&my_string[0..6]);
+//     let word = first_word(&my_string[..]);
+//     // `first_word` also works on references to `String`s, which are equivalent
+//     // to whole slices of `String`s.
+//     let word = first_word(&my_string);
+
+//     let my_string_literal = "hello world";
+
+//     // `first_word` works on slices of string literals, whether partial or
+//     // whole.
+//     let word = first_word(&my_string_literal[0..6]);
+//     let word = first_word(&my_string_literal[..]);
+
+//     // Because string literals *are* string slices already,
+//     // this works too, without the slice syntax!
+//     let word = first_word(my_string_literal);
+
+//     println!("{}", word);
+// }
+
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    println!("{:?}", slice);
+}
